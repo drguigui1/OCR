727d78013cf3943a42ca924ef91bbf74f607b3c2
KoHann <alexandre.vermeulen@laposte.net>
2018-09-29 10:47:49 +0200

    implémentation add_matrix et hadamar_product

--- source/matrix/Matrix.c
+++ source/matrix/Matrix.c
@@ -5,20 +5,20 @@
 typedef struct Matrix
 {
 	int rows;
-	int column;
+	int columns;
 	int *pt;
 }Matrix;
 
-Matrix init_matrix(int rows, int column);
+Matrix init_matrix(int rows, int columns);
 Matrix add_matrix(Matrix A, Matrix B);
 Matrix hadamar_product(Matrix C, Matrix D);
 
 int main(void)
 {
 	/*Matrix m = init_matrix(5,5);
-	*(m.pt + m.column + 1) = 5;
+	*(m.pt + m.columns + 1) = 5;
 	for (int i = 0; i < m.rows; i++)
-		for (int j = 0; j < m.column; j++)
+		for (int j = 0; j < m.columns; j++)
 			printf("%d ",*(m.pt + i*m.column + j));
 	printf("\n");*/
 	return 0;
@@ -27,14 +27,14 @@ int main(void)
 /*---------------------------------------*/
 
 //Matrix initialization
-Matrix init_matrix(int rows, int column)
+Matrix init_matrix(int rows, int columns)
 {
-	Matrix M = {rows,column,(int *)malloc(rows * column * sizeof(int))};	
+	Matrix M = {rows,columns,(int *)malloc(rows * columns * sizeof(int))};	
 
 	//init
 	for (int i = 0; i < rows; i++)
-		for(int j = 0; j < column; j++)
-			*(M.pt + i*column + j) = 0; //replace with Gaussian distribution
+		for(int j = 0; j < columns; j++)
+			*(M.pt + i*columns + j) = 0; //replace with Gaussian distribution
 
 	return M;
 }
@@ -44,8 +44,10 @@ Matrix init_matrix(int rows, int column)
 //matrix addition
 Matrix add_matrix(Matrix A, Matrix B)
 {
-	Matrix M = {A.rows,A.column,(int *)malloc(A.rows * A.column * sizeof(int))};
-	//
+	Matrix M = {A.rows,A.columns,(int *)malloc(A.rows * A.columns * sizeof(int))};
+	for (int i = 0; i < rows; i++)
+		for(int j = 0; j < columns; j++)
+			*(M.pt + i*columns + j) = *(A.pt + i*columns + j) + *(B.pt + i*columns + j);
 	return M;
 }
 
@@ -55,8 +57,10 @@ Matrix add_matrix(Matrix A, Matrix B)
 Matrix hadamar_product(Matrix A, Matrix B)
 {
 
-	Matrix M = {A.rows,A.column,(int *)malloc(A.rows * A.column * sizeof(int))};
-	//
+	Matrix M = {A.rows,A.columns,(int *)malloc(A.rows * A.columns * sizeof(int))};
+	for (int i = 0; i < rows; i++)
+		for(int j = 0; j < columns; j++)
+			*(M.pt + i*columns + j) = *(A.pt + i*columns + j) * *(B.pt + i*columns + j);
 	return M;
 }
 
